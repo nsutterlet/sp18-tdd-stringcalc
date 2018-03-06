8cc1197f0457b3fa4e4af070c4a773981bded026
Ulysse Rosselet <urosselet@gmail.com>
2018-03-06 18:58:09 +0100

    delimiters on first line test passes

--- src/main/java/ch/hearc/ig/odi/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/StringCalculator.java
@@ -6,15 +6,27 @@ package ch.hearc.ig.odi;
 
 import static java.lang.Integer.parseInt;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
   StringCalculator() {
   }
 
-  public int add(String numbers) {
+  public int add(String input) {
     int sum = 0;
+    String numbers = input;
+    String delimiters = "[,\n]"; // standard delimiters
+    Pattern pattern = Pattern
+        .compile("(?<=//)(.*)(?=\n)"); // capture positive lookbehind, positive lookahead
+    Matcher m = pattern.matcher(input);
+    if (m.find()) {
+      delimiters = m.group(1);
+      numbers = numbers.substring(m.end(1) + 1);
+    }
     if (!numbers.isEmpty()) {
-      String[] operands = numbers.split("[,\n]");
+      String[] operands = numbers.split(delimiters);
       for (String operand : operands) {
         sum += parseInt(operand);
       }
