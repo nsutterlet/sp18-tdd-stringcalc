fe5f7868e3cddc19f6804d1b9d8445b4327ad70b
Ulysse Rosselet <urosselet@gmail.com>
2018-03-06 15:47:15 +0100

    n numbers test passes

--- src/test/java/ch/hearc/ig/odi/StringCalculatorTest.java
+++ src/test/java/ch/hearc/ig/odi/StringCalculatorTest.java
@@ -33,6 +33,9 @@ public class StringCalculatorTest {
         {"", 0}, // emptyString
         {"1", 1}, // addASingleNumber
         {"1,2", 3}, // addTwoNumbers
+        {"1,2,3,4", 10}, // add n Numbers
+        {"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30", 465},
+        // add n Numbers
     });
   }
 
