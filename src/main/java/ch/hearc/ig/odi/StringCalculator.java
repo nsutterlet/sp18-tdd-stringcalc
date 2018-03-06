f45f9c6dc36f98b2cdb6955f758364a377ca7f1a
Ulysse Rosselet <urosselet@gmail.com>
2018-03-06 19:32:31 +0100

    refactoring: inner class to extract delimiters from input

--- src/main/java/ch/hearc/ig/odi/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/StringCalculator.java
@@ -16,15 +16,9 @@ public class StringCalculator {
 
   public int add(String input) {
     int sum = 0;
-    String numbers = input;
-    String delimiters = "[,\n]"; // standard delimiters
-    Pattern pattern = Pattern
-        .compile("(?<=//)(.*)(?=\n)"); // capture positive lookbehind, positive lookahead
-    Matcher m = pattern.matcher(input);
-    if (m.find()) {
-      delimiters = m.group(1);
-      numbers = numbers.substring(m.end(1) + 1);
-    }
+    CalcConfig calcConfig = new CalcConfig(input).invoke();
+    String numbers = calcConfig.getNumbers();
+    String delimiters = calcConfig.getDelimiters();
     if (!numbers.isEmpty()) {
       String[] operands = numbers.split(delimiters);
       for (String operand : operands) {
@@ -34,4 +28,35 @@ public class StringCalculator {
     return sum;
   }
 
+  private static class CalcConfig {
+
+    private String input;
+    private String numbers;
+    private String delimiters;
+
+    CalcConfig(String input) {
+      this.input = input;
+    }
+
+    String getNumbers() {
+      return numbers;
+    }
+
+    String getDelimiters() {
+      return delimiters;
+    }
+
+    CalcConfig invoke() {
+      delimiters = "[,\n]";
+      numbers = input;
+      Pattern pattern = Pattern
+          .compile("(?<=//)(.*)(?=\n)"); // capture positive lookbehind, positive lookahead
+      Matcher m = pattern.matcher(input);
+      if (m.find()) {
+        delimiters = m.group(1);
+        numbers = numbers.substring(m.end(1) + 1);
+      }
+      return this;
+    }
+  }
 }
