191dc0a54f0b0cff6d9a0de0bb31ffd10717a985
Ulysse Rosselet <urosselet@gmail.com>
2018-03-06 15:49:02 +0100

    new lines between numbers test fails

--- src/test/java/ch/hearc/ig/odi/StringCalculatorTest.java
+++ src/test/java/ch/hearc/ig/odi/StringCalculatorTest.java
@@ -36,6 +36,7 @@ public class StringCalculatorTest {
         {"1,2,3,4", 10}, // add n Numbers
         {"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30", 465},
         // add n Numbers
+        {"1\n2,3", 6} // allow new lines between
     });
   }
 
