69da018333cd42a99140b9ea3eb68925c7b3e133
Ulysse Rosselet <urosselet@gmail.com>
2018-03-07 10:09:55 +0100

    delimiters of any length test passes

--- src/main/java/ch/hearc/ig/odi/StringCalculator.java
+++ src/main/java/ch/hearc/ig/odi/StringCalculator.java
@@ -70,8 +70,29 @@ public class StringCalculator {
       if (m.find()) {
         delimiters = m.group(1);
         numbers = numbers.substring(m.end(1) + 1);
+        Pattern p2 = Pattern
+            .compile("(?<=\\[)(.*)(?=\\])");
+        Matcher m2 = p2.matcher(delimiters);
+        if (m2.find()) {
+          delimiters = escapeSpecialRegexChars(m2.group(1));
+        }
       }
       return this;
     }
+
+    /**
+     * Escapes all special regex characters
+     * REF: https://stackoverflow.com/a/25853507/2195344
+     *
+     * @param str input
+     * @return string with special regex characters escaped
+     */
+    String escapeSpecialRegexChars(String str) {
+      Pattern SPECIAL_REGEX_CHARS = Pattern.compile("[{}()\\[\\].+*?^$\\\\|]");
+      return SPECIAL_REGEX_CHARS.matcher(str).replaceAll("\\\\$0");
+    }
+
   }
+
+
 }
